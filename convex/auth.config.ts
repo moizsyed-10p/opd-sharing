3b9ef096d34a7a541516268c0b7b33473056b673
moizsyed-10p <moiz.syed@10pearls.com>
2026-05-17 23:53:42 +0500

    move covex url to env

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://finer-bulldog-6.clerk.accounts.dev",
+      domain: process.env.CLERK_FRONTEND_API_URL!,
       applicationID: "convex",
     },
   ],
